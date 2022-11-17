f3568d446d7a31521fb868d98f6a3cf0d9bc3e74
Anh Tuan <anhtuan128@gmail.com>
2022-11-18 05:47:30 +1100

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'dc6cb3afeab5f1fe862f');
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+
+import './shared/variables.scss';
+
+ReactDOM.render(<Home/>, document.querySelector("#render-react-example-here"))
